019215b91ba1177f61617d4c33147b19217ad292
Lutvi Rosyady <moorails@gmail.com>
2021-12-06 18:34:50 +0700

    Updating Trading Strategy

--- instance.js
+++ instance.js
@@ -2,61 +2,14 @@ const c = (module.exports = {});
 
 c.symbols = [
   {
-    symbol: 'ADAUSDT',
+    symbol: 'SANDUSDT',
     exchange: 'binance_futures',
-    periods: ['1m', '15m', '1h'],
-    trade: {
-      currency_capital: 2,
-      strategies: [
-        {
-          strategy: 'dip_catcher',
-          interval: '15m',
-          options: {
-            period: '15m'
-          }
-        }
-      ]
+    extra: {
+        binance_leverage: 20
     },
-    watchdogs: [
-      {
-        name: 'risk_reward_ratio',
-        target_percent: 3.1,
-        stop_percent: 2.1
-      }
-    ]
-
-  },
-  {
-    symbol: 'MATICUSDT',
-    exchange: 'binance_futures',
     periods: ['1m', '15m', '1h'],
     trade: {
-      currency_capital: 2,
-      strategies: [
-        {
-          strategy: 'dip_catcher',
-          interval: '15m',
-          options: {
-            period: '15m'
-          }
-        }
-      ]
-    },
-    watchdogs: [
-      {
-        name: 'risk_reward_ratio',
-        target_percent: 3.1,
-        stop_percent: 2.1
-      }
-    ]
-
-  },
- {
-    symbol: 'MANAUSDT',
-    exchange: 'binance_futures',
-    periods: ['1m', '15m', '1h'],
-    trade: {
-      currency_capital: 2,
+      currency_capital: 5,
       strategies: [
         {
           strategy: 'dip_catcher',
