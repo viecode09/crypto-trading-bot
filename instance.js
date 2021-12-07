bb1a20d2f47342d6d3a8be66ed5e833fcb086d25
Lutvi Rosyady <moorails@gmail.com>
2021-12-07 11:27:18 +0700

    Adding More Coin to Config

--- instance.js
+++ instance.js
@@ -4,9 +4,126 @@ c.symbols = [
   {
     symbol: 'SANDUSDT',
     exchange: 'binance_futures',
-    extra: {
-        binance_leverage: 20
+    periods: ['1m', '15m', '1h'],
+    trade: {
+      currency_capital: 10,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
     },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.1,
+        stop_percent: 2.1
+      }
+    ]
+  },
+  {
+    symbol: 'MANAUSDT',
+    exchange: 'binance_futures',
+    periods: ['1m', '15m', '1h'],
+    trade: {
+      currency_capital: 10,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.1,
+        stop_percent: 2.1
+      }
+    ]
+  },
+  {
+    symbol: 'ADAUSDT',
+    exchange: 'binance_futures',
+    periods: ['1m', '15m', '1h'],
+    trade: {
+      currency_capital: 10,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.1,
+        stop_percent: 2.1
+      }
+    ]
+  },
+  {
+    symbol: 'XLMUSDT',
+    exchange: 'binance_futures',
+    periods: ['1m', '15m', '1h'],
+    trade: {
+      currency_capital: 10,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.1,
+        stop_percent: 2.1
+      }
+    ]
+  },
+  {
+    symbol: 'GALAUSDT',
+    exchange: 'binance_futures',
+    periods: ['1m', '15m', '1h'],
+    trade: {
+      currency_capital: 10,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.1,
+        stop_percent: 2.1
+      }
+    ]
+  },
+  {
+    symbol: 'DOTUSDT',
+    exchange: 'binance_futures',
     periods: ['1m', '15m', '1h'],
     trade: {
       currency_capital: 20,
