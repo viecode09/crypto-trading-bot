3c24b4dd13a22e88e685045b9251b68bf2b55971
Lutvi Rosyady <moorails@gmail.com>
2021-12-08 17:51:30 +0700

    Testing Combining Strategy

--- instance.js
+++ instance.js
@@ -30,7 +30,7 @@ c.symbols = [
     exchange: 'binance_futures',
     periods: ['5m', '15m', '1h'],
     trade: {
-      currency_capital: 200,
+      currency_capital: 300,
       strategies: [
         {
           strategy: 'dip_catcher',
@@ -38,13 +38,29 @@ c.symbols = [
           options: {
             period: '15m'
           }
-        }
+        },
+          {
+              strategy: 'obv_pump_dump',
+              interval: '15m'
+          },
+          {
+              strategy: 'cci',
+              interval: '15m'
+          },
+          {
+              strategy: 'dca_dipper',
+              interval: '15m'
+          },
+          {
+              strategy: 'macd',
+              interval: '15m'
+          }
       ]
     },
     watchdogs: [
       {
         name: 'risk_reward_ratio',
-        target_percent: 3.0,
+        target_percent: 2.0,
         stop_percent: 8.0
       }
     ]
