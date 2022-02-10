0f33e20647ef2aef370834aa07877c8f07e1aa78
Lutvi Rosyady <moorails@gmail.com>
2022-02-10 10:46:56 +0700

    Updating Instance

--- instance.js
+++ instance.js
@@ -3,30 +3,38 @@ const c = (module.exports = {});
 c.symbols = []
 
 let z = [
-    'MATICUSDT', 'NEARUSDT', 'XLMUSDT', 'ADAUSDT', 'SANDUSDT'
+    'MATICUSDT', 'NEARUSDT', 'GALAUSDT', 'ADAUSDT', 'SANDUSDT'
 ]
 
 z.forEach((pair) => {
     c.symbols.push({
         'symbol': pair,
-        'periods': ['15m', '1h', '4h'],
+        'periods': ['5m', '15m', '1h'],
         'exchange': 'binance_futures',
         'trade': {
-            'currency_capital': 100,
+            'currency_capital': 200,
             'strategies': [
-               {
-                    'strategy': 'obv_pump_dump',
-                    'interval': '15m',
-                    'options': {
-                        'period': '15m'
-                    }
-                }
+                {
+                  'strategy': 'cci',
+                  'options': {
+                      'period': '5m'
+                  }
+              },
+              {
+                  'strategy': 'obv_pump_dump'
+              },
+              {
+                  'strategy': 'macd',
+                  'options': {
+                      'period': '15m'
+                  }
+              }
             ]
         },
         'watchdogs': [
         {
             'name': 'risk_reward_ratio',
-            'target_percent': 2.1,
+            'target_percent': 1.0,
             'stop_percent': 3.0
         }
         ]
